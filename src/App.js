a75d5331d757ea4b4b3d52b7be57c0a43b9af54f
Dismus Ng'eno <dismuskiplimo@gmail.com>
2022-09-15 00:42:05 +0300

    Improved components File/Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import { Header, Footer, Features, Blog, Possibility, WhatGPT3 } from './containers';
+import { CTA, Navbar, Brand } from './components';
+
 const App = () => {
   return (
-    <div>
-        Hello World
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
