99dcc63690db77cc6980a091566a7d3b324cae4e
Dismus Ng'eno <dismuskiplimo@gmail.com>
2022-09-15 00:59:30 +0300

    Initial CSS definitions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Header, Footer, Features, Blog, Possibility, WhatGPT3 } from './containers';
 import { CTA, Navbar, Brand } from './components';
+import './App.css';
 
 const App = () => {
   return (
