326444d3e233f80c53a5be8008f903c442eb286d
Dismus Ng'eno <dismuskiplimo@gmail.com>
2022-09-16 11:45:26 +0300

    Updated Navbar CSS

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
 
 import './navbar.css';
 
@@ -7,8 +8,30 @@ function Navbar() {
   return (
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
-        <div className='gpt__navbar-links-logo'>
+        <div className='gpt3__navbar-links_logo'>
           <img src={logo} alt="logo" />
+        </div>
+
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+
+          <p>
+            <a href="#wgpt3">What is GPT3?</a>
+          </p>
+
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+
+          <p>
+            <a href="#blog">Library</a>
+          </p>
         </div>
       </div>
     </div>
