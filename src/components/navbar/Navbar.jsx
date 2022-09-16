bbfb0aa38934c37b2f139e345d3ca16d27f47f6c
Dismus Ng'eno <dismuskiplimo@gmail.com>
2022-09-16 11:24:54 +0300

    Added Assets

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 
 import './navbar.css';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt__navbar-links-logo'>
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
   )
 }
 
