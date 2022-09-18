540d598b6f4b1d57250fc63954df8da4bc0eacd2
Dismus Ng'eno <dismuskiplimo@gmail.com>
2022-09-19 00:29:23 +0300

    Adding styling to header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 import './header.css';
 
-function Header() {
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
+
+const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id = 'home'>
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Something amazing with GPT-3 OpenAi
+        </h1>
+
+        <p>
+        Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.
+        </p>
+
+        <div className="gpt3__header-content__input">
+          <input type="email" name="" id="" placeholder = 'Your email address' />
+          <button type = "button">Get Started</button>
+        </div>
+
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+
+        <div className="gpt3__header-image">
+          <img src={ai} alt="ai" />
+        </div>
+      </div>
+    </div>
   )
 }
 
